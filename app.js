5d95b513a1c8f6dffb65a0ca8800f1a24bc6b91e
UTSAV MANDHANI <85448958+Utsav2929@users.noreply.github.com>
2023-08-20 12:17:23 +0530

    created server

---
+++ app.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+const app = express();
+dotenv.config();
+app.use(bodyParser.json({limit: '30mb',extended:true}));
+// PORT=5000
+// CONNECTION_URL=mongodb+srv://utsav2929:utsav2929@cluster0.agsr6z5.mongodb.net/?retryWrites=true&w=majority
+app.use(bodyParser.urlencoded({limit: '30mb',extended:true}));
+app.use(cors());
+
+
+
+
+const PORT =process.env.PORT;
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+
+
+
+
+mongoose.connection.on('error',err=>{
+    console.log('connection failed');
+  });
+  
+  mongoose.connection.on('connected',()=>{
+    console.log('connected successfully with database');
+  });
+  
+  app.get('*',(req,res)=>{
+      res.status(200).json({
+        message:'bad request'
+      })
+    })
+   export default app;
+
+
+
+// mongoose.set('useFindAndModify', false); 
---
+++ server.js
@@ -0,0 +1,12 @@
+import  http  from 'http';
+import app from './app.js'
+
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+server.listen(port, () => {
+    console.log("SERVER RUNNING");
+  });
+  
+
